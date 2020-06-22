ad13155651d7c085c6e1cad39d97808c2008816a
tamayoram <tamayoram@gmail.com>
2020-06-21 19:09:13 -0500

    CRUD Base de datos Biblioteca junio 2020

--- Android_06_2020/MiBiblioteca/app/src/main/java/com/example/mibibliotecabasedatos/BaseDatos/DataBaseReserva.java
+++ Android_06_2020/MiBiblioteca/app/src/main/java/com/example/mibibliotecabasedatos/BaseDatos/DataBaseReserva.java
@@ -11,7 +11,7 @@ import android.service.restrictions.RestrictionsReceiver;
 import com.example.mibibliotecabasedatos.tablas.Reserva;
 
 public class DataBaseReserva extends DataBaseAccess {
-    private static String[] allcolumns={Reserva.COLUMNA_ID,Reserva.COLUMNA_LIBRO,Reserva.COLUMNA_AFILIADO,Reserva.COLUMNA_FEENTREGA}; // vector con la información de campos para libro
+    private static String[] allcolumns={Reserva.COLUMNA_ID,Reserva.COLUMNA_AFILIADO,Reserva.COLUMNA_LIBRO,Reserva.COLUMNA_FEENTREGA}; // vector con la información de campos para libro
 
     public DataBaseReserva(Context context)
     {
@@ -19,47 +19,88 @@ public class DataBaseReserva extends DataBaseAccess {
 
     }
     //grabar (create)
-    public Reserva IngresarReserva(String libro, String afiliado, String feentrega)
+    public Reserva IngresarReserva(String afiliado,String libro, String feentrega)
     {
         ContentValues values=new ContentValues();
-        values.put(Reserva.COLUMNA_LIBRO,libro); // esta instrucciones son para insertar los valores en la base de datos
         values.put(Reserva.COLUMNA_AFILIADO,afiliado);
+        values.put(Reserva.COLUMNA_LIBRO,libro); // esta instrucciones son para insertar los valores en la base de datos
         values.put(Reserva.COLUMNA_FEENTREGA,feentrega);
 
         long insertId= database.insert(Reserva.NOMBRE_TABLA,null,values); // instrucción para grabar los values de arriba.
 
-        return null; // retornar una respuesta que ya quedo grabada.
+        Cursor cursor = database.query(Reserva.NOMBRE_TABLA,
+                allcolumns, Reserva.COLUMNA_ID + " = " + insertId,
+                null,
+                null, null, null);
+        cursor.moveToFirst();
+        Reserva newReserva = cursorToReserva(cursor);
+        cursor.close();
+        return newReserva; // retornar una respuesta que ya quedo grabada.
     }
 
     // borrar (delete)
-    public void borrarReserva(String id)
+    public Reserva borrarReserva(String id)
     {
-        database.delete(Reserva.NOMBRE_TABLA,Reserva.COLUMNA_ID+"="+id,null); // instrucción para borar, el null es para decir que no voy a utilizar más condiciones.
+        database.delete(Reserva.NOMBRE_TABLA,Reserva.COLUMNA_ID + " = " + id,null); // instrucción para borar, el null es para decir que no voy a utilizar más condiciones.
         //Si necesito otra condiciones las voy agregando y cierro con null cuando finalice.
+        return null;
     }
 
     // actualizar (update)
-    public void actualizarReserva(String id,String libro, String afiliado, String feentrega)
+    public Reserva actualizarReserva(String id, String afiliado, String libro, String feentrega)
     {
         ContentValues values=new ContentValues();
-        values.put(Reserva.COLUMNA_LIBRO,libro); // esta instrucciones son para insertar los valores en la base de datos
         values.put(Reserva.COLUMNA_AFILIADO,afiliado);
+        values.put(Reserva.COLUMNA_LIBRO,libro); // esta instrucciones son para insertar los valores en la base de datos
         values.put(Reserva.COLUMNA_FEENTREGA,feentrega);
 
-        database.update(Reserva.NOMBRE_TABLA,values,Reserva.COLUMNA_ID+"="+id,null);
+        database.update(Reserva.NOMBRE_TABLA,values,Reserva.COLUMNA_ID + " = " + id,null);
+
+        return null;
     }
 
     //consultar o leer (read)
 
-    public Reserva getLibro(String id)
+    public Reserva getReserva(String id)
     {
-        Reserva libro= null; //inicialización del objeto libro
-        Cursor cursor= database.query(Reserva.NOMBRE_TABLA, allcolumns,Reserva.COLUMNA_ID+"="+id,null,null,null,null); // un cursor es una tabla
+        Reserva reserva= null; //inicialización del objeto libro
+        Cursor cursor= database.query(Reserva.NOMBRE_TABLA, allcolumns,Reserva.COLUMNA_ID + " = " + id,null,null,null,null); // un cursor es una tabla
         // se define tabla, todas las columnas, se filtra por ID, los null es para definir los argumentos que faltan
 
-        cursor.close(); // se cierra la consulta
-        return libro; //se devuelve la tabla
+        cursor.moveToFirst();
+        if (!cursor.isAfterLast()) {
+            reserva = cursorToReserva(cursor);
+        }
 
+        cursor.close(); // se cierra la consulta
+        return reserva; //se devuelve la tabla
+
+    }
+
+    // código de la librería cursor
+    public List<Reserva> getAllReserva() {
+        List<Reserva> reservas = new ArrayList<Reserva>();
+        Cursor cursor = database.query(Reserva.NOMBRE_TABLA,
+                allcolumns, null, null, null,
+                null, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Reserva reserva = cursorToReserva(cursor);
+            reservas.add(reserva);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return reservas;
+    }
+
+    private static Reserva cursorToReserva(Cursor cursor) {
+        Reserva reserva = new Reserva();
+        reserva.setId(cursor.getString(0));
+        reserva.setAfiliado(cursor.getString(1));
+        reserva.setLibro(cursor.getString(2));
+        reserva.setFeentrega(cursor.getString(3));
+
+        return reserva;
     }
 
 }
--- Android_06_2020/MiBiblioteca/app/src/main/java/com/example/mibibliotecabasedatos/LReserva.java
+++ Android_06_2020/MiBiblioteca/app/src/main/java/com/example/mibibliotecabasedatos/LReserva.java
@@ -4,11 +4,114 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button; //librería para manipular botones
+import android.widget.EditText; //librería para manipular edit text
+import android.view.View.OnClickListener; //librería para eventos
+import java.sql.SQLException; // permite capturar los errores de la base de datos
+
+import com.example.mibibliotecabasedatos.BaseDatos.DataBaseReserva; //Clase donde se encuentran las instrucciones para el CRUD
+import com.example.mibibliotecabasedatos.tablas.Reserva;
+
 public class LReserva extends AppCompatActivity {
+
+    private DataBaseReserva dbReserva; // permite administrar las instrucciones del CRUD
+    String seleccionReserva=""; //variable para mostrar mensajes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_l_reserva);
+
+        dbReserva= new DataBaseReserva(getApplicationContext()); // instancia la variable dbAfiliado para que abra la BD
+        try
+        {
+            dbReserva.open(); // se abre la base de datos y se capturan errores con la estructura try catch
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace(); // imprime mensaje de alerta si se presenta un error
+        }
+
+        Button oBtnCrearReserva=(Button) findViewById(R.id.btnCrearReserva);
+        oBtnCrearReserva.setOnClickListener(crearReserva);// definición del método para crear.
+
+        Button oBtnConsultarReserva=(Button) findViewById(R.id.btnConsultarReserva);
+        oBtnConsultarReserva.setOnClickListener(consultarReserva); //definición del método para consultar.
+
+        Button oBtnEditarReserva=(Button) findViewById(R.id.btnEditarReserva);
+        oBtnEditarReserva.setOnClickListener(EditarReserva); //definición del método para consultar.
+
+        Button oBtnEliminarReserva=(Button) findViewById(R.id.btnEliminarReserva);
+        oBtnEliminarReserva.setOnClickListener(EliminarReserva); //definición del método para consultar.
+
+        Button oBtnLimpiarReserva=(Button) findViewById(R.id.btnLimpiarReserva);
+        oBtnLimpiarReserva.setOnClickListener(limpiarReserva);// metodo para limpiar los controles.
     }
+
+    //Crear
+    public OnClickListener crearReserva =new OnClickListener() { // instrucciones para programar el evento click cuando es con base de datos
+        @Override
+        public void onClick(View v) {
+            Reserva oReservaCrear= dbReserva.IngresarReserva(
+                    ((EditText) findViewById(R.id.etAfiliadoReserva)).getText().toString(),
+                    ((EditText) findViewById(R.id.etLibroReserva)).getText().toString(),
+                    ((EditText) findViewById(R.id.etFeEntregaReserva)).getText().toString()
+            );
+        }
+    };
+
+    //Editar
+
+    public OnClickListener EditarReserva =new OnClickListener() { // instrucciones para programar el evento click cuando es con base de datos
+        @Override
+        public void onClick(View v) {
+            Reserva oReservaEditar= dbReserva.actualizarReserva(
+                    ((EditText) findViewById(R.id.etIdReserva)).getText().toString(),
+                    ((EditText) findViewById(R.id.etAfiliadoReserva)).getText().toString(),
+                    ((EditText) findViewById(R.id.etLibroReserva)).getText().toString(),
+                    ((EditText) findViewById(R.id.etFeEntregaReserva)).getText().toString()
+
+            );
+        }
+    };
+
+    //Consultar
+    public OnClickListener consultarReserva=new OnClickListener() { // instrucciones para programar el evento click cuando es con base de datos
+        @Override
+        public void onClick(View v) {
+            Reserva oReservaConsultar=dbReserva.getReserva(
+                    ((EditText) findViewById(R.id.etIdReserva)).getText().toString());
+
+            // Se muestran los datos en cada uno de los campos
+            ((EditText) findViewById(R.id.etAfiliadoReserva)).setText(oReservaConsultar.getAfiliado());
+            ((EditText) findViewById(R.id.etLibroReserva)).setText(oReservaConsultar.getLibro());
+            ((EditText) findViewById(R.id.etFeEntregaReserva)).setText(oReservaConsultar.getFeentrega());
+
+        }
+    };
+
+    //Eliminar
+
+    public OnClickListener EliminarReserva=new OnClickListener() { // instrucciones para programar el evento click cuando es con base de datos
+        @Override
+        public void onClick(View v) {
+            Reserva oReservaEliminar=dbReserva.borrarReserva(
+                    ((EditText) findViewById(R.id.etIdReserva)).getText().toString());
+        }
+    };
+
+    //Limpiar
+
+    public OnClickListener limpiarReserva=new OnClickListener() { // instrucciones para programar el evento click cuando es con base de datos
+        @Override
+        public void onClick(View v) {
+
+            ((EditText) findViewById(R.id.etAfiliadoReserva)).setText("");
+            ((EditText) findViewById(R.id.etLibroReserva)).setText("");
+            ((EditText) findViewById(R.id.etFeEntregaReserva)).setText("");
+
+        }
+    };
 }
